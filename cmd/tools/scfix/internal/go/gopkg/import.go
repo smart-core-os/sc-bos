c20eea1293486ee3e2ea37d5464f6fdff930b414
Matt Nathan <matt.nathan@vanti.co.uk>
2026-01-09 14:26:13 +0000

    tools/scfix: extract gopkg.ImportPathToAssumedName
    This util was in the goprotoimports fixer but other go fixers will likely need it over time.

---
+++ cmd/tools/scfix/internal/go/gopkg/import.go
@@ -0,0 +1,36 @@
+package gopkg
+
+import (
+	"path"
+	"strconv"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
+// ImportPathToAssumedName returns the assumed package name of an import path.
+// This is copied from x/tools /internal/imports/fix.go.
+func ImportPathToAssumedName(importPath string) string {
+	base := path.Base(importPath)
+	if strings.HasPrefix(base, "v") {
+		if _, err := strconv.Atoi(base[1:]); err == nil {
+			dir := path.Dir(importPath)
+			if dir != "." {
+				base = path.Base(dir)
+			}
+		}
+	}
+	base = strings.TrimPrefix(base, "go-")
+	if i := strings.IndexFunc(base, notIdentifier); i >= 0 {
+		base = base[:i]
+	}
+	return base
+}
+
+// notIdentifier reports whether ch is an invalid identifier character.
+func notIdentifier(ch rune) bool {
+	return !('a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' ||
+		'0' <= ch && ch <= '9' ||
+		ch == '_' ||
+		ch >= utf8.RuneSelf && (unicode.IsLetter(ch) || unicode.IsDigit(ch)))
+}
