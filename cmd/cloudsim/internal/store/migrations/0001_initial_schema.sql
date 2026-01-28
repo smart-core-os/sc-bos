f71822154317505945cfc48a98f97facffb694eb
Matt Randell <matt.randell@vanti.co.uk>
2026-01-28 16:15:59 +0000

    cloudsim: storage logic for new tool for simulating cloud-managed config
    Add skeleton of new server cmd/cloudsim - not yet a working API server.
    Intended for testing and development, not as a production server.
    
    Add package cloudsim/store, a SQLite-based storage layer using similar patterns to the account package.

---
+++ cmd/cloudsim/internal/store/migrations/0001_initial_schema.sql
@@ -0,0 +1,49 @@
+CREATE TABLE sites (
+    id              INTEGER PRIMARY KEY,
+    name            TEXT NOT NULL,
+    create_time     DATETIME NOT NULL,
+
+    CONSTRAINT create_time_format CHECK ( create_time IS datetime(create_time, 'subsec') )
+);
+
+CREATE TABLE nodes (
+    id              INTEGER PRIMARY KEY,
+    hostname        TEXT NOT NULL,
+    site_id         INTEGER NOT NULL,
+    create_time     DATETIME NOT NULL,
+
+    FOREIGN KEY (site_id) REFERENCES sites (id) ON DELETE CASCADE,
+    CONSTRAINT create_time_format CHECK ( create_time IS datetime(create_time, 'subsec') )
+);
+
+CREATE INDEX nodes_site_id ON nodes (site_id);
+
+CREATE TABLE config_versions (
+    id              INTEGER PRIMARY KEY,
+    node_id         INTEGER NOT NULL,
+    version_number  TEXT NOT NULL,
+    payload         BLOB NOT NULL,
+    create_time     DATETIME NOT NULL,
+
+    FOREIGN KEY (node_id) REFERENCES nodes (id) ON DELETE CASCADE,
+    CONSTRAINT create_time_format CHECK ( create_time IS datetime(create_time, 'subsec') )
+);
+
+CREATE UNIQUE INDEX config_versions_node_version ON config_versions (node_id, version_number);
+CREATE INDEX config_versions_node_id ON config_versions (node_id);
+
+CREATE TABLE deployments (
+    id                  INTEGER PRIMARY KEY,
+    config_version_id   INTEGER NOT NULL,
+    status              TEXT NOT NULL,
+    start_time          DATETIME NOT NULL,
+    finished_time       DATETIME,
+
+    FOREIGN KEY (config_version_id) REFERENCES config_versions (id) ON DELETE CASCADE,
+    CONSTRAINT status_valid CHECK ( status IN ('PENDING', 'IN_PROGRESS', 'COMPLETED', 'FAILED') ),
+    CONSTRAINT start_time_format CHECK ( start_time IS datetime(start_time, 'subsec') ),
+    CONSTRAINT finished_time_format CHECK ( finished_time IS NULL OR finished_time IS datetime(finished_time, 'subsec') )
+);
+
+CREATE INDEX deployments_config_version_id ON deployments (config_version_id);
+CREATE INDEX deployments_status ON deployments (status);
