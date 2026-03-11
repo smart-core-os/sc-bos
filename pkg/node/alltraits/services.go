3847eebef2c054d55878982a2da43d998c70935d
Dean Redfern <dean.redfern@vanti.co.uk>
2026-03-11 09:11:46 +0000

    fix service ordering

--- pkg/node/alltraits/services.go
+++ pkg/node/alltraits/services.go
@@ -38,9 +38,9 @@ import (
 	gen_mqttpb "github.com/smart-core-os/sc-bos/pkg/proto/mqttpb"
 	"github.com/smart-core-os/sc-bos/pkg/proto/occupancysensorpb"
 	"github.com/smart-core-os/sc-bos/pkg/proto/reportpb"
+	gen_resourceusepb "github.com/smart-core-os/sc-bos/pkg/proto/resourceusepb"
 	"github.com/smart-core-os/sc-bos/pkg/proto/securityeventpb"
 	gen_serviceticketpb "github.com/smart-core-os/sc-bos/pkg/proto/serviceticketpb"
-	gen_resourceusepb "github.com/smart-core-os/sc-bos/pkg/proto/resourceusepb"
 	gen_soundsensorpb "github.com/smart-core-os/sc-bos/pkg/proto/soundsensorpb"
 	"github.com/smart-core-os/sc-bos/pkg/proto/statuspb"
 	gen_temperaturepb "github.com/smart-core-os/sc-bos/pkg/proto/temperaturepb"
@@ -91,8 +91,8 @@ var serviceRegistry = map[trait.Name][]grpc.ServiceDesc{
 	healthpb.TraitName:         {gen_healthpb.HealthApi_ServiceDesc, gen_healthpb.HealthHistory_ServiceDesc},
 	meter.TraitName:            {meterpb.MeterApi_ServiceDesc, meterpb.MeterInfo_ServiceDesc, meterpb.MeterHistory_ServiceDesc},
 	mqttpb.TraitName:           {gen_mqttpb.MqttService_ServiceDesc},
-	report.TraitName:                  {reportpb.ReportApi_ServiceDesc},
-	resourceusepb.TraitName:   {gen_resourceusepb.ResourceUseApi_ServiceDesc, gen_resourceusepb.ResourceUseHistory_ServiceDesc},
+	report.TraitName:           {reportpb.ReportApi_ServiceDesc},
+	resourceusepb.TraitName:    {gen_resourceusepb.ResourceUseApi_ServiceDesc, gen_resourceusepb.ResourceUseHistory_ServiceDesc},
 	securityevent.TraitName:    {securityeventpb.SecurityEventApi_ServiceDesc},
 	serviceticketpb.TraitName:  {gen_serviceticketpb.ServiceTicketApi_ServiceDesc, gen_serviceticketpb.ServiceTicketInfo_ServiceDesc},
 	soundsensorpb.TraitName:    {gen_soundsensorpb.SoundSensorApi_ServiceDesc, gen_soundsensorpb.SoundSensorInfo_ServiceDesc},
