e0049d1a81289741373005e41ce79dc3526123da
Matt Randell <matt.randell@vanti.co.uk>
2026-03-23 15:36:15 +0000

    remove sc-api and sc-bos directories, relocating docs
    Move docs from sc-api/ into docs/
    
    Delete sc-api & sc-golang.
    
    Remove scripts/repo-merge.sh which is no longer needed now the repos are
    merged.

--- sc-api/go/publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-if [ $1 = "perform" ]; then
-  echo "git tag go/$2"
-  git tag "go/v$2"
-fi
--- sc-api/node/generate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-rm -rf ./proto/info ./proto/traits ./proto/types
-yarn run gen
