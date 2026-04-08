0ee2fcee093f65eee8494052c53d1d6cdf0a2a68
deanredfern26 <deanredfern26@gmail.com>
2026-04-08 12:17:11 +0100

    driver/mock: simulate ambient humidity

--- pkg/driver/mock/auto/airtemperature.go
+++ pkg/driver/mock/auto/airtemperature.go
@@ -3,9 +3,9 @@ package auto
 import (
 	"context"
 	"math"
+	"math/rand/v2"
 	"time"
 
-	"math/rand/v2"
 	"google.golang.org/protobuf/types/known/fieldmaskpb"
 
 	"github.com/smart-core-os/sc-bos/pkg/proto/airtemperaturepb"
@@ -21,6 +21,7 @@ func AirTemperatureAuto(model *airtemperaturepb.Model) *service.Service[string] 
 			randomNumber := 18 + rand.Float64()*6
 			// give each device a random set point between 18 and 24 with .05 degree accuracy
 			setPoint := math.Round(randomNumber*2) / 2
+			initialHumidity := float32(30 + rand.Float64()*40) // 30–70%
 			state := &airtemperaturepb.AirTemperature{
 				AmbientTemperature: &typespb.Temperature{
 					ValueCelsius: setPoint + (rand.Float64()*4 - 2),
@@ -28,6 +29,7 @@ func AirTemperatureAuto(model *airtemperaturepb.Model) *service.Service[string] 
 				TemperatureGoal: &airtemperaturepb.AirTemperature_TemperatureSetPoint{
 					TemperatureSetPoint: &typespb.Temperature{ValueCelsius: setPoint},
 				},
+				AmbientHumidity: &initialHumidity,
 			}
 			_, _ = model.UpdateAirTemperature(state)
 			for {
@@ -40,8 +42,16 @@ func AirTemperatureAuto(model *airtemperaturepb.Model) *service.Service[string] 
 						setPoint = state.GetTemperatureSetPoint().ValueCelsius
 						// update the ambient to be +- 2 degrees from the set point
 						state.AmbientTemperature.ValueCelsius = setPoint + (rand.Float64()*4 - 2)
+						// drift humidity by ±2%
+						humidity := state.GetAmbientHumidity() + float32(rand.Float64()*4-2)
+						if humidity < 0 {
+							humidity = 0
+						} else if humidity > 100 {
+							humidity = 100
+						}
+						state.AmbientHumidity = &humidity
 						_, _ = model.UpdateAirTemperature(state, resource.WithUpdateMask(&fieldmaskpb.FieldMask{
-							Paths: []string{"ambient_temperature.value_celsius"},
+							Paths: []string{"ambient_temperature.value_celsius", "ambient_humidity"},
 						}))
 					}
 				}
