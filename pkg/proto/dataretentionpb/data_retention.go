e69e205f35d32edd261818d8860a5bdb8a5780ff
deanredfern26 <deanredfern26@gmail.com>
2026-05-05 14:07:23 +0100

    fix trait name

--- pkg/proto/dataretentionpb/data_retention.go
+++ pkg/proto/dataretentionpb/data_retention.go
@@ -2,4 +2,4 @@ package dataretentionpb
 
 import "github.com/smart-core-os/sc-bos/pkg/trait"
 
-const TraitName trait.Name = "smartcore.bos.data_retention.DataRetention"
+const TraitName trait.Name = "smartcore.bos.dataretention"
