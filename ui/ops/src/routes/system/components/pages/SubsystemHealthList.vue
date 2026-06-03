4ec06bcfb5588ce82d4213f2209669c6579fd561
Dean Redfern <dean.redfern@vanti.co.uk>
2026-06-03 14:15:40 +0100

    ui/ops: add CSV health status export to Components and Subsystem Health

--- ui/ops/src/routes/system/components/pages/SubsystemHealthList.vue
+++ ui/ops/src/routes/system/components/pages/SubsystemHealthList.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    <h3 class="text-h3 pt-2 pb-6">Subsystems</h3>
+    <div class="d-flex align-center mb-0">
+      <h3 class="text-h3 pt-2 pb-6 flex-grow-1">Subsystems</h3>
+      <v-tooltip v-if="subsystems.length > 0" location="bottom">
+        <template #activator="{ props }">
+          <v-btn
+              class="mt-n3"
+              icon="mdi-download"
+              size="small"
+              :loading="exporting"
+              v-bind="props"
+              @click="onExportReport">
+            <v-icon size="24"/>
+          </v-btn>
+        </template>
+        Export Report
+      </v-tooltip>
+    </div>
 
     <div v-if="subsystems.length > 0" class="d-flex flex-wrap ml-n2">
       <subsystem-health-card
@@ -21,9 +37,20 @@
 
 <script setup>
 import SubsystemHealthCard from '@/routes/system/components/SubsystemHealthCard.vue';
+import {downloadSubsystemHealthReport} from './subsystemHealthExport.js';
 import {useUiConfigStore} from '@/stores/uiConfig.js';
-import {computed} from 'vue';
+import {computed, ref} from 'vue';
 
 const uiConfig = useUiConfigStore();
 const subsystems = computed(() => uiConfig.getOrDefault('system.subsystemHealth.subsystems', []));
+
+const exporting = ref(false);
+const onExportReport = async () => {
+  exporting.value = true;
+  try {
+    await downloadSubsystemHealthReport(subsystems.value);
+  } finally {
+    exporting.value = false;
+  }
+};
 </script>
